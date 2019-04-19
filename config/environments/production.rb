0eb1d1b2a9cc813b25ba13e4301b8960ab6d9037
daveed <david.alphen@gmail.com>
2019-04-19 09:54:01 -0700

    production action cable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://sheltered-plains-10101.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://sheltered-plains-10101.herokuapp.com', 'http://sheltered-plains-10101.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
